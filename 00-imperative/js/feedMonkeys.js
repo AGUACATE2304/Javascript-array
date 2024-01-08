a81cb74d54e1f6cca441688ee136b66d329d9ae6
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2024-01-08 20:20:22 +0100

    Complete feedMonkeys

--- 00-imperative/js/feedMonkeys.js
+++ 00-imperative/js/feedMonkeys.js
@@ -8,4 +8,15 @@
 
 const MONKEYS = ["🐒", "🦍", "🦧"];
 
+export const feedMonkeys = function (fruit) {
+  const monkeys = [];
+  const collectionLenth = MONKEYS.length;
 
+  for (let index = 0; index < collectionLenth; index++) {
+    const monkey = `${MONKEYS[index]} ${fruit}`;
+
+    monkeys.push(monkey);
+  }
+
+  return monkeys;
+};
